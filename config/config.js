de78c7be275fb49a3a8de2a951cffbeb4a8c4007
Michael <quasar.exe@hotmail.com>
2015-08-21 17:31:08 +1000

    Fix sidebar contact link targets

--- config/config.js
+++ config/config.js
@@ -12,10 +12,13 @@ exports.blogDesc = 'a rookie content management platform written in an attempt t
     'full-stack (MongoDB, Express, Angular and Node) as well as Bootstrap and a bit of jQuery.';
 
 /** Contact information/links */
+var NONE = '#'; // constant meaning no link for this social media
 exports.contacts = {
     twitter: "http://www.twitter.com/",
     linkedin: "http://www.linkedin.com/",
-    facebook: "http://www.facebook.com/",
-    youtube: "http://www.youtube.com/",
+    facebook: 'http://www.facebook.com/',
+    youtube: 'http://www.youtube.com/',
     email: "address@domain.com"
 };
+
+exports.NONE = NONE;
--- controllers/view.controller.js
+++ controllers/view.controller.js
@@ -6,13 +6,25 @@ var config = require('../config/config');
 
 exports.sidebar = function(req, res) {
     var theme = ThemeModel.getTheme(req);
+
+    var contactsNewWindow = [];
+    var newWindow = 'target="_blank"';
+    for (var media in config.contacts) {
+        if (config.contacts[media] != config.NONE) {
+            contactsNewWindow[media] = newWindow; // if not N/A, set to _blank
+        } else {
+            contactsNewWindow[media] = ''; // otherwise no target="_blank"
+        }
+    }
+
     res.render('sidebar', {
         theme: theme,
 
         // contact links
-        contacts: config.contacts
+        contacts: config.contacts,
+        contactsNewWindow: contactsNewWindow
     });
-}
+};
 
 exports.blogPost = function(req, res) {
     res.render('blog_post');
